9eef05a178a1ed4d4be0595ca936447ef7003303
Henry Agu <aguhenrychuks@gmail.com>
2025-04-30 21:02:48 +0100

    codebase structure set!

---
+++ src/routes/__root.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import Navbar from '../components/layout/Navbar'
+import Footer from '../components/layout/Footer'
+
+export const Route = createRootRoute({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <React.Fragment>
+      <Navbar/>
+      <Outlet />
+      <Footer/>
+    </React.Fragment>
+  )
+}
