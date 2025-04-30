f3350ddb96cd1c9d217e38794168635829507f2e
Henry Agu <aguhenrychuks@gmail.com>
2025-05-01 00:08:52 +0100

    feat: navbar desktop

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,47 @@
+import { Link } from "@tanstack/react-router";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  type NavMenu = {
+    title: string;
+    path: string;
+  };
 
-export default Navbar
+  const navMenu: NavMenu[] = [
+    { title: "Home", path: "/" },
+    { title: "News", path: "/news" },
+    { title: "About Us", path: "/about" },
+    { title: "Contact Us", path: "/contact" },
+  ];
+
+  return (
+    <nav className="px-5 lg:px-30 py-6 flex items-center justify-between font-roboto">
+      <img src="/icons/Logo.svg" alt="logo" />
+      <ul className="flex items-center gap-x-7.5">
+        {navMenu.map((item, index) => (
+          <li key={index}>
+            <Link
+              to={item.path}
+              activeProps={{
+                className: "text-brand-green-900 font-bold",
+              }}
+              inactiveProps={{
+                className: "text-base font-normal text-brand-gray-500 transition-color duration-200 ease-in hover:text-brand-green-900",
+              }}
+            >
+              {item.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="flex items-center gap-x-7.5">
+        <div className="flex items-center gap-x-3 bg-secondary-100 p-2 pl-4 rounded-[5px]">
+          <input type="text" placeholder="Search" className="text-secondary-400 text-base font-normal focus:outline-secondary-100 w-full"/>
+          <img src="/icons/search.svg" alt="search_icon"/>
+        </div>
+        <div className="">1</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
