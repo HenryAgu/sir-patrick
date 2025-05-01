3002a4aa9bdcd6b8c6ccc0c0ef971df32ae797e4
Henry Agu <aguhenrychuks@gmail.com>
2025-05-01 23:28:35 +0100

    fix: UI

--- src/components/shared/socialMenu.tsx
+++ src/components/shared/socialMenu.tsx
@@ -14,13 +14,13 @@ const SocialMenu = () => {
     { title: "instagram", image: "/icons/insta.svg", path: "/" },
   ];
   return (
-    <section className="flex items-center gap-x-2.5 lg:gap-x-3.5 mt-3">
+    <section className="flex items-center gap-x-1.5 lg:gap-x-3.5 mt-3">
       {socialMenu.map((social, index) => (
         <Link to={social.path} key={index}>
           <img
             src={social.image}
             alt={social.title}
-            className="lg:w-auto lg:h-auto w-[23.67px] h-[23.67px] opacity-[80%]"
+            className="lg:w-auto lg:h-auto w-[18.57px] h-[18.57px] opacity-[80%]"
           />
         </Link>
       ))}
