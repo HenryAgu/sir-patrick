f1a07094079640c1b06e4b08239c540b49d474c8
Henry Agu <aguhenrychuks@gmail.com>
2025-05-05 00:48:45 +0100

    fix: slug page for mobile

--- src/components/Blog/comments.tsx
+++ src/components/Blog/comments.tsx
@@ -2,7 +2,7 @@ import CommentForm from "./commentForm"
 
 const Comments = () => {
   return (
-    <section className="mx-auto container w-full font-roboto bg-brand-green-100 pt-10 pb-12 lg:pt-20 lg:pb-24 lg:px-60 px-3.5">
+    <section className="mx-auto container w-full font-roboto bg-brand-green-100 pt-10 pb-12 lg:pt-20 lg:pb-24 lg:px-14 xl:px-30 2xl:px-60 px-3.5">
     <div className="flex flex-col gap-y-1 lg:gap-y-2">
       <p className="text-secondary-800 lg:text-[31px] font-semibold lg:leading-7 text-base">Join the Conversation and Ask your Questions</p>
       <span className="text-secondary-600 text-sm lg:text-xl lg:leading-8 font-normal">Your email and other personal details is not made visible.</span>
