73b36d4b4e15ce9a61a91a6dedc95f414ca151a1
Henry Agu <aguhenrychuks@gmail.com>
2025-05-04 23:53:48 +0100

    feat: comment form for mobile

--- src/components/Blog/commentForm.tsx
+++ src/components/Blog/commentForm.tsx
@@ -1,36 +1,36 @@
 const CommentForm = () => {
   return (
-    <form className="my-8 flex flex-col gap-y-4.5">
+    <form className="my-8 flex flex-col gap-y-[9px] lg:gap-y-4.5">
       <textarea
         name="message"
         id=""
-        className="resize-none h-[309px] bg-white rounded-[10px] px-5 py-10 text-secondary-600 text-xl leading-8 font-normal"
+        className="resize-none h-[140px] lg:h-[309px] bg-white rounded-[10px] px-2.5 py-4.5 lg:px-5 lg:py-10 text-secondary-600 text-sm lg:text-xl lg:leading-8 font-normal border-[0.45px] border-brand-gray-100"
         placeholder="Type here"
         required
       />
       <input
         type="text"
         name="text"
-        className="bg-white rounded-[10px] px-5 py-10 text-secondary-600 text-xl leading-8 font-normal"
+        className="bg-white rounded-[10px] px-2.5 py-4.5 lg:px-5 lg:py-10 text-secondary-600 text-sm lg:text-xl lg:leading-8 font-normal border-[0.45px] border-brand-gray-100"
         placeholder="Name*"
         required
       />
       <input
         type="email"
-        className="bg-white rounded-[10px] px-5 py-10 text-secondary-600 text-xl leading-8 font-normal"
+        className="bg-white rounded-[10px] px-2.5 py-4.5 lg:px-5 lg:py-10 text-secondary-600 text-sm lg:text-xl lg:leading-8 font-normal border-[0.45px] border-brand-gray-100"
         placeholder="Email*"
         required
       />
-      <div className="flex items-center gap-x-4 mb-10">
+      <div className="flex items-center gap-x-2 lg:gap-x-4 mb-5 lg:mb-10">
         <input
           type="checkbox"
-          className="w-[18px] h-[18px] border border-black rounded-sm bg-white checked:border-transparent focus:outline-none"
+          className="lg:w-[18px] lg:h-[18px] w-[10px] h-[10px] border border-black rounded-sm bg-white checked:border-transparent focus:outline-none"
         />
-        <span className="text-secondary-600 text-xl leading-8 font-normal">
+        <span className="text-secondary-600 text-xs lg:text-xl leading-8 font-normal">
           Notify me of any response
         </span>
       </div>
-      <button className="bg-brand-green-900 w-fit py-5 px-11 rounded-[12px] text-center text-white text-[31px] font-normal cursor-pointer transition duration-200 ease-in hover:opacity-50">
+      <button className="bg-brand-green-900 w-fit py-3.5 lg:py-5 px-5 lg:px-11 rounded-[6px] lg:rounded-[12px] text-center text-white text-sm lg:text-[31px] font-normal cursor-pointer transition duration-200 ease-in hover:opacity-50">
         Post comment
       </button>
     </form>
--- src/routes/blog/$slug.tsx
+++ src/routes/blog/$slug.tsx
@@ -16,7 +16,7 @@ function RouteComponent() {
         {/* Header */}
         <section className="flex flex-col gap-y-3.5 lg:gap-y-5">
           <h1 className="font-semibold text-xl leading-7 lg:text-[39px] text-secondary-800 lg:leading-10">
-            How to secure a good PPA whereever you are posted to for NYSC
+            How to secure a good PPA wherever you are posted to for NYSC
           </h1>
           <div className="flex items-center gap-x-7">
             <div className="flex items-center gap-x-2">
